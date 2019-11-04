71e91fde7ddcd9771d2ac22a96bdd01e367d4961
maliahavlicek <malia.havlicek@gmail.com>
2019-11-03 22:52:08 -0700

    use jQuery to swap out hero background image

--- assets/js/heroImage.js
+++ assets/js/heroImage.js
@@ -1,26 +1,39 @@
 /*
-    Hero Image Changes image every 2 seconds
+    Hero Image Changes image every 3 seconds
  */
 var myIndex = 0;
-setTimeout(function(){ carousel(myIndex) }, 2000);
+setTimeout(function(){ carousel(myIndex) }, 3000);
 
 
 
 function carousel(myIndex){
+  var directory = "assets/images/hero/lg/";
+
   var images=[
-  {image: "assets/images/hero/1_web_novigrad-kod-zadra--ivan-coric_0.jpg", alt: "Stunning image of Novigrad near Zadar at night."},
-  {image: "assets/images/hero/01-DSC_2891_Sakarun_A.Gospic.jpg", alt: "People on Holiday enjoying Sakraun Beach."} ,
-  {image: "assets/images/hero/01-split_-ivan-coric-2013.jpg", alt:"People gathering at St. Nikolas cathederal at dusk in Split."}
+  {image: directory+"split.jpg", alt: "Split downtown coast at night."},
+  {image: directory+"klis.jpg", alt: "Klis fortress."} ,
+  {image: directory+"mljet.jpg", alt:"Mljet national park, blue cove beach."},
+  {image: directory+"pula.jpg",alt:"Pula amplitheater."},
+  {image: directory+"krka.jpg",alt:"The magical waterfalls of Krka National Park."},
+  {image: directory+"trakoscan.jpg",alt:"13th Century Castle Troakoscan in the northern part of Croatia."},
+  {image: directory+"dugi-otok.jpg",alt:"The cliffs at the edge of Dugi Otok where on a clear day you supposedly can see Italy."},
+  {image: directory+"01-split.jpg",alt:"Diocletian's Palace, built for the Roman emperor in AD 305."},
+  {image: directory+"hidden-bay_Dubrovnik.jpg",alt:"Hidden bay off the beaten path in Dubrovnik."},
+  {image: directory+"plitvice-lakes-np.jpg",alt:"Plitvice Lakes National park is full of waterfalls, clear lakes of stunning blues and nature trails."},
+  {image: directory+"istria.jpg",alt:"Stunning sunset over a coastal town in the Istria region."}
 
-  ]
+  ];
 
-  var x = document.getElementById("carasoule");
-  var image_tag =`<img class="slide" src='${images[myIndex].image}' alt = '${images[myIndex].alt}'/>`;
-  x.innerHTML=image_tag;
+
+  var image_url = images[myIndex].image;
+  var alt_text = images[myIndex].alt;
+
+  $("#carasouleImg").css("background-image", "url(" + image_url + ")");
+
 
   myIndex++;
   if (myIndex >= images.length) {
     myIndex = 0
   }
-  setTimeout(function(){ carousel(myIndex) }, 2000);
+  setTimeout(function(){ carousel(myIndex) }, 3000);
 }
